078a6a4d0eab04fb635c128b60d35b17b3f2d8e4
Edin Imsirovic <edin.imsirovic.work@gmail.com>
2021-08-24 17:33:41 -0400

    Improveed Regex

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-const movieReviewFormat = /^.+[(][12][0-9][0-9][0-9][)] ((:biohazard:\s?){1}|(:star:\s?){1,5}|(:star2:\s?){5})\n{2}/g
+// const movieReviewFormat = /^.+[(][12][0-9][0-9][0-9][)] ((:biohazard:\s?){1}|(:star:\s?){1,5}|(:star2:\s?){5})\n{2}/g
+const movieReviewFormat = /^.+[(][12]\d{3}[)] ((:biohazard:\s?){1}|(:star:\s?){1,5}|(:star2:\s?){5})\n{2}/g
